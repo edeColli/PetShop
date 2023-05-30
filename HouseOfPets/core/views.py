1f8d9fae7474e91f32c67fb669adf18a2b032c76
edeColli <ednesio.colli@gmail.com>
2023-05-30 19:11:14 -0300

    incluida a autenticação do usuario

--- HouseOfPets/core/views.py
+++ HouseOfPets/core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
 from core.forms import ContatoForm
 from core.forms import ReservaForm
 from .models import Reserva
@@ -66,3 +72,57 @@ def reserva(request):
 
 def sobre(request):
     return render(request, 'about.html')
+
+
+def signup(request):
+  SIGNUP ='signup.html'
+
+  if request.method == 'GET':
+    return render(request, SIGNUP, {
+      'form': UserCreationForm
+    })
+  else:
+    if request.POST['password1'] == request.POST['password2']:
+        try:
+          print('objeto')
+          user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+          user.save()
+          login(request, user)
+          return redirect('../signin')
+        except:
+          return render(request, SIGNUP, {
+            'form': UserCreationForm,
+            'error': 'E-mail já cadastrado'
+          })
+    return render(request, SIGNUP, {
+      'form': UserCreationForm,
+      'error': 'Senhas são diferentes'
+    })
+
+
+def signin(request):
+  SIGNIN ='signin.html'
+
+  if (request.method == 'GET'):
+    return render(request, SIGNIN, {
+      'form': AuthenticationForm
+    })
+  else:
+    user = authenticate(request, username=request.POST['username'],password=request.POST['password'])
+    if user is None:
+      return render(request, 'signin.html', {
+          'form': AuthenticationForm,
+          'error': 'Usuário ou senha inválido'
+      })
+    else:
+      login(request, user)
+      return redirect('../dashboard')
+
+@login_required
+def sair(request):
+  logout(request)
+  return redirect('../')
+
+@login_required
+def dashboard(request):
+  return render(request, 'dashboard.html')
