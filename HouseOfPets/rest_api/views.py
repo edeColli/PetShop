280fb99e265e43714a2c9f4d2143a94ea7d097d6
Ednesio <ednesio.colli@gmail.com>
2023-06-20 21:02:42 -0300

    Aula2006

--- HouseOfPets/rest_api/views.py
+++ HouseOfPets/rest_api/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.viewsets import ModelViewSet
+from core.models import Reserva
+from rest_api.serializers import AgendamentoModelSerializer
+from datetime import date
 # Create your views here.
+
+class AgendamentoModelViewSet(ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = AgendamentoModelSerializer
+
+class AgendamentoModelViewSetFinalizados(ModelViewSet):
+    queryset = Reserva.objects.all().filter(isFinalizado=True)
+    serializer_class = AgendamentoModelSerializer
+
+class AgendamentoModelViewSetAtivos(ModelViewSet):
+    queryset = Reserva.objects.all().filter(isFinalizado=False)
+    serializer_class = AgendamentoModelSerializer
+
+class AgendamentoModelViewSetDoDia(ModelViewSet):
+    dataAtual = date.today()
+    queryset = Reserva.objects.all().filter(data=dataAtual)
+    serializer_class = AgendamentoModelSerializer
+
 @api_view(['GET','POST'])
 def hello_world(request):
     if request.method == 'POST':
